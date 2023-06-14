0860cc95084c04f015b948e121c56ba72dda6062
Aakash Yadav <aakashkilhor@gmail.com>
2023-06-14 09:41:41 +0530

    1.1

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const app = express()
 app.use(express.json()) 
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 // import auth from ("./middleware/auth")
 // Custom middleware
@@ -63,52 +69,54 @@ app.post("/register", async (req,res)=>{
    }   
 })
 
-app.post("/login", async(req,res)=>{
-    try {
-        // Collect information from frontend
-        const { email, password} = req.body
-        // Validate
-        if (!(email&&password)) {
-            res.status(401).send("Email & password are required")
-        }
-        // Check user existence
-        const user = await User.findOne({email})
+    app.post("/login", async(req,res)=>{
+        try {
+            // Collect information from frontend
+            const { email, password} = req.body
+            // Validate
+            if (!(email&&password)) {
+                res.status(401).send("Email & password are required")
+            }
+            // Check user existence
+            const user = await User.findOne({email})
+            console.log("Hello")
+            console.log(user)
+            console.log("Hi")
+            // If user does not exist
+            // console.log(user._id)
+            if (!user){
+                res.send("User does not exist")
+            }
 
-        // If user does not exist
-        if (!user){
-            res.send("User does not exist")
-        }
+            // Match the password Create and send the token
+            if (user && (await bcrypt.compare(password, user.password))) {
+                const token = jwt.sign({id:user._id,name:user.firstname,email},'secret',{expiresIn:'12h'})
+            
+                user.password = undefined
+                console.log(token)
+                // user.token = token
 
-        // Match the password Create and send the token
-        if (user && (await bcrypt.compare(password, user.password))) {
-            const token = jwt.sign({id:user._id,email},'secret',{expiresIn:'2h'})
-        
-            user.password = undefined
-            // user.token = token
-
-        const options = {
-            expires : new Date(Date.now() + 3*24*60*60*1000),
-            httpOnly : true
+            // res.cookie("token", token, options);
+            // console.log("Line 92");
+            res.status(200).cookie("token",token).json({
+                success:true,
+                token,
+                user
+            })
+            }
+            // res.sendStatus(400).send("email or password is incorrect")
+            
+            
+            
+        } catch (error) {
+            console.log(error)
+            console.log("Error in login")
         }
-        console.log("Line 92");
-        res.status(200).cookie("token",token,options).json({
-            success:true,
-            token,
-            user
-        })
-        }
-        res.sendStatus(400).send("email or password is incorrect")
-        
-        
-        
-    } catch (error) {
-        console.log(error)
-        console.log("Error in login")
-    }
-})
+    })
 
 app.get("/dashboard", auth, (req,res) => {
-    res.send(`Welcome to dashboard ${req.user.name}`) 
+    console.log(req.user.name)
+    res.send(req.user.name)    
 })
 
 
